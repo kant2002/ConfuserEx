e4bcbe4ce21e76e154164690482fd0e19baec523
Andrii Kurdiumov <kant2002@gmail.com>
2026-02-21 14:51:38 +0100

    Update TFM to net472

---
+++ build.ps1
@@ -0,0 +1,8 @@
+param(
+    [string]$Configuration = "Release",
+    [string]$SolutionPath = "Confuser2.slnx"
+)
+
+$ErrorActionPreference = 'Stop'
+msbuild $SolutionPath /t:Restore,Build,Test /p:Configuration=$Configuration /p:Platform=x64 /v:m
+vstest.console Tests/*/bin/Release/net*/*.dll /TestAdapterPath:Tests/*/bin/Release/net* /Logger:trx
