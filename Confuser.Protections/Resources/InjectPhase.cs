3250483e11d15d6a14d1f85a2f994fab3f77595f
Andrii Kurdiumov <kant2002@gmail.com>
2026-03-14 15:07:06 +0100

    Remove duplicate transformation (#23)
    Fixed leftover from #22

--- Confuser.Protections/Resources/InjectPhase.cs
+++ Confuser.Protections/Resources/InjectPhase.cs
@@ -136,8 +136,6 @@ namespace Confuser.Protections.Resources {
 				repl.AddRange(arg);
 				repl.Add(Instruction.Create(OpCodes.Dup));
 				repl.Add(Instruction.Create(OpCodes.Ldtoken, moduleCtx.DataField));
-				repl.Add(Instruction.Create(OpCodes.Call, moduleCtx.Module.Import(
-					typeof(RuntimeHelpers).GetMethod("InitializeArray"))));
 				var runtimeHelpers = moduleCtx.Module.CorLibTypes.GetTypeRef("System.Runtime.CompilerServices", "RuntimeHelpers");
 				var arrayTypeRef = moduleCtx.Module.CorLibTypes.GetTypeRef("System", "Array");
 				var runtimeFieldHandle = moduleCtx.Module.CorLibTypes.GetTypeRef("System", "RuntimeFieldHandle");
