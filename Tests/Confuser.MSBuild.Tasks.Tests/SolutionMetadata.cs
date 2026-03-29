9fa142f4b0000b83635ea049786e2a98b1ff3eca
Andrii Kurdiumov <kant2002@gmail.com>
2026-03-29 18:11:07 +0200

    Add tests for building using MSBuild files

---
+++ Tests/Confuser.MSBuild.Tasks.Tests/SolutionMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TruePath;
+
+namespace Confuser.MSBuild.Tasks.Tests {
+	internal class SolutionMetadata {
+		public static AbsolutePath SourceRoot => new(ResolvedAttribute.SourceRoot);
+		public static AbsolutePath ArtifactsRoot => new(ResolvedAttribute.ArtifactsRoot);
+		public static string VersionPrefix => ResolvedAttribute.VersionPrefix;
+
+		private static SolutionMetadataAttribute ResolvedAttribute =>
+			typeof(SolutionMetadata).Assembly.GetCustomAttribute<SolutionMetadataAttribute>()
+			?? throw new Exception($"Missing {nameof(SolutionMetadataAttribute)} metadata attribute.");
+
+	}
+}
---
+++ Tests/Confuser.MSBuild.Tasks.Tests/SolutionMetadataAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confuser.MSBuild.Tasks.Tests {
+	internal class SolutionMetadataAttribute : Attribute {
+		public string SourceRoot { get; }
+		public string ArtifactsRoot { get; }
+		public string VersionPrefix { get; }
+
+		public SolutionMetadataAttribute(string sourceRoot, string artifactsRoot, string versionPrefix) {
+			SourceRoot = sourceRoot;
+			ArtifactsRoot = artifactsRoot;
+			VersionPrefix = versionPrefix;
+		}
+	}
+}
